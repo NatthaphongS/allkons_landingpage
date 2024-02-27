0a007fe53fa8be6df2cd7e505e433cd211c69435
NatthaphongS <sinsupharoek@gmail.com>
2024-02-27 14:10:33 +0700

    loadscript

---
+++ src/loadParticle.js
@@ -0,0 +1,24 @@
+// Dynamically load the "particles.js" script
+function loadParticlesScript() {
+  const script = document.createElement('script');
+  script.src = 'particles.js';
+  script.async = true;
+  document.body.appendChild(script);
+  const script2 = document.createElement('script');
+  script2.src = 'particles-config.js';
+  script2.async = true;
+  document.body.appendChild(script2);
+
+  script.onload = () => {
+    // Script has loaded
+    console.log('Particles.js script loaded successfully.');
+    // Here you can initialize particles.js as needed
+  };
+
+  script.onerror = (error) => {
+    console.error('Error loading the particles.js script:', error);
+  };
+}
+
+// Call this function early in your app's initialization process
+export default loadParticlesScript;
--- src/main.jsx
+++ src/main.jsx
@@ -2,5 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
+import loadParticlesScript from './loadParticle.js';
+
+loadParticlesScript();
 
 ReactDOM.createRoot(document.getElementById('root')).render(<App />);
